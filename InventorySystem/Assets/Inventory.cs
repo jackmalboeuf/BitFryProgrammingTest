7e133a8267f35fdd359785e6897988b997c6ba51
JackMalboeuf <jmalboeuf70@gmail.com>
2019-06-04 15:11:15 -0500

    First

---
+++ InventorySystem/Assets/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public Item currentItem;
+    public Text displayText;
+
+    public void SelectItem(Item item)
+    {
+        currentItem = item;
+        displayText.text = "Selected Item: " + currentItem.itemName;
+    }
+}
